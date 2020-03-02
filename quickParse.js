f8528703e095ced18eb3fd06e9c870ba0ef3a905
unknown <harryvu801@gmail.com>
2020-03-01 22:28:49 -0700

    quick commit

--- quickParse.js
+++ quickParse.js
@@ -4,8 +4,9 @@ const papa  = require('papaparse');
 
 const allMakes = ["Suzuki","Honda","Yamaha","Polaris","Adly Moto","Aeon","AJS","All American Choppers","Alouette","AlphaSports","Alta Motors","Amen Chassis Works","American Classic Motors","American Dirt Bike","American IronHorse","American Lifan Industry","American Motorcycle Corp.","Anarchy Motorcycles","APC Motorcycle Company","Apollo Choppers","Aprilia","Arch Motorcycle Company","Arctic Cat","Argo Atv","Ariel","ATK","Backroad Choppers","Bajaj","Benelli","Bennche","BETA","Big Bear Choppers","Big Boyz Custom Cycles","Big Dog","Big Inch Bikes","Big Tony's Chopp Shop","Bimota","Black Swamp Choppers","BMC","BMS MOTORSPORTS","BMW","Boa-Ski","Bobcat","Bolens","Bombardier","Borella","Boss Hoss","Bourget's Bike Works","Brammo","Bridgestone","Brough Superior","BSA","Buell","Bultaco","Cagiva","California Customs","California Scooter Co.","California Sidecar-Trike","Campagna","Can-Am","Cannondale","Carefree Custom Cycles","Carolina Custom Products","Case IH","CC Cycles","CCM","Cectek","CF-Moto","Champion Sidecars","Chaparral","Cheetah Trikes","Chop Shop Customs","Chopper Nation","Christini AWD Motorcycles","Chuck Wagon","Cleveland CycleWerks","ClubCar","Coast 2 Coast Choppers","Cobra Motorcycle","Cobra Trike","COLEMAN POWERSPORTS","Columbia","Confederate Motorcycle","Coonyz Customz","Covington Cycle City","CPI USA","CQR","Create A Custom Cycle","Cruise Dynamic","Cub Cadet","Cubi-K Scooters","Cushman","Cushman II","CZ","D.R.R.","Daelim","Darwin Motorcycles","Death Row Motorcycles","Demon Choppers","Denver Choppers","Derbi","Desperado","Detroit Brothers","Diablo","Diamo","Dirico Motorcycles USA","DKW/MZ","Dominion Motorcycle","Ducati","DURUXX","E-TON","EBR","Ecstasy","Ego Cycle","Energica Motor Company","Enfield","Evinrude","Excelsior-Henderson","EZ Go","EZ-Go","F.B.I. Motor Co.","Fischer","Flyrite Choppers","Flyscooters","FUZION MOTORS","GAS GAS","GEM","Genuine Scooter Co.","Glycing Motor Corp","Grandeur Manufacturing","Greeves","Hammerhead Off-Road","Hannigan","Harley-Davidson","Hisun Motors Corp USA","Hodaka","Husaberg","Husqvarna","Hyosung","Ice Bear Powersports","Illusion Cycles","Independence","Indian","Indian Motorcycle-Gilroy","Intrepid Cycles","Iron Eagle","Ironworks","Italika","Italjet (Childrens Minis)","Jawa/CZ","Jim Nasi Customs","John Deere","Johnny Pag","Johnson","Joyner USA","Kandi USA","Kaotic Customs","Kasea","Kawasaki","Kayo USA","Keeway","Kinetic","KIOTI","KTM","Kubota","KYMCO","Lambretta","Lance","Land Pride","Las Vegas Trikes","Laverda","Legends Motorsports","Lehman Trikes","LEM","Logic Motor Company","Mahindra","Maico","Manta","Marauder","Marrelli","Masai","Maserati","Massimo","Matchless","Mercury","Midwest Motor Vehicles","Milwaukee","Montesa","Moto Guzzi","Moto Morini","Moto-ski","Motor Trike","Motor-Piper","Motus Motorcycles","Mustang","MV Agusta","MZ","Natalia Scooters","Ness Motorcycles","New Holland","Norton","O.C. Choppers","Odes UTVs","Old School Motorcycle Co","Oreion Motors","Ossa","Pagsta Motors","Paramount Custom Cycles","Paul Yaffe Originals","Peirspeed","Penton","PGO","Piaggio","Polini","Power Chrome","Precision Cycle Works","Precision Powersports","Predator Motor Corp","Pro-One","Proper Chopper","QLINK","QTEC-ENGINEERING","Quadzilla","Raider","Rebel West Powersports","Recreatives Industries","Red Horse Motorworks","Redcat Motors","Redline","Redneck","Renault","Revolution Cycle Company","Rewaco","Rhino Motorcycles","Rickman","Ridley Motorcycle","Rival Motorsports","Road Hog","Roadsmith","Rokon","Royal Enfield","RTX","Rucker Performance","Rupp","S.P.C.N. Motorcycles","Sabertooth Motorcycles","Saxon Motorcycle","Schwinn","Scorpion Motorsports","SDG","Sea-Doo","Sears","Sebring Auto-Cycle","Sherco","Ski-Daddler","Ski-Doo","Ski-Whiz","Skiroule","Sno-Jet","Sno-Pony","Southwest Choppers","Spencer Bowman Customs","SS Trike","SSR Motorsports","Star EV","Steed Musclebike","Streamline Designs","Strociek","Suckerpunch Sallys","Supermach","Surgical-Steeds","Swift Motor Co.","SWM","SYM","T.G.B.","Tank Sports","Tanom Motors","Textron Off Road","The Auto Moto","Thoroughbred","Thunder Bikes","Thunder Mountain Cycles","TigerShark","Titan Motorcycle Co.","Tomberlin","Tomberlin Outdoor","Tomcar","Tomos","Trifun","Triumph","Twist-N-Go","Ultra Cycle","Unison Motors","United Motors","Ural","V8 Choppers","Vanderhall","Vectrix","Velocette","Veloz","Vengeance Motorcycles","Vento","Vespa","Victory","Viking","Vincent","Viper","Von Dutch Kustom Cycles","VOR","Wet Jet","Wild West","Wrath Manufacturing","Xtreme","XY Powersports","Yankee","ZAP","Zero","Zero Engineering","Zongshen","TM","Accurate Cycle Engineering","Lightning","Thunderbolt","HM"]
 
-const filePath = path.resolve('productListVersions/productsVer6.csv')
+const filePath = path.resolve('productsVer6.csv')
 const file = fs.createReadStream(filePath);
+const regexMarkers = [/2x4/, /4x4/, /6x6/, /8x8/, /2X2/, /4X4/, /6X6/, /8x8/,]
 
 const list1 = [];
 const list2 = [];
@@ -20,43 +21,11 @@ papa.parse(file, {
       let fitArr = row['Product Name'].split(' for ')[1].split(' ')
       fitArr.pop();
       let mm = fitArr.join(' ');
-      // console.log(mm)
-      let newMm;
-      // console.log(mm)
-      if (mm.includes('2x4')) {
-        newMm = mm.replace('2x4', '')
-      }
-      if (mm.includes('4x4')) {
-        newMm = mm.replace('4x4', '')
-      }
-      if (mm.includes('6x6')) {
-        newMm = mm.replace('6x6', '')
-      }
-      if (mm.includes('8x8')) {
-        newMm = mm.replace('8x8', '')
-      }
-      if (mm.includes('2X4')) {
-        newMm = mm.replace('2X4', '')
-      }
-      if (mm.includes('4X4')) {
-        newMm = mm.replace('4X4', '')
-      }
-      if (mm.includes('6X6')) {
-        newMm = mm.replace('6X6', '')
-      }
-      if (mm.includes('8X8')) {
-        newMm = mm.replace('8X8', '')
-      }
-      if (mm.includes(' /w')) {
-        newMm = mm.slice(0, mm.indexOf(' w/'))
-      }
-      if (mm.includes('(')) {
-        let slice = mm.slice(mm.indexOf('('), mm.indexOf(')')+1)
-        newMm = mm.replace(slice, '')
-      }
-      if(newMm) data[index]['Product Name'] = data[index]['Product Name'].replace(mm, newMm)
-      console.log(data[index]['Product Name'])
+      // console.log(mm);
 
+      if(/2x4|2X4|4x4|4X4|6x6|6X6|8x8|8X8/.test(mm)) {
+        console.log(mm)
+      }
       // let inMakes = false
       // if(!list2.includes(mm)) {
       //   let added = false;
@@ -79,20 +48,20 @@ papa.parse(file, {
     // list1.sort().forEach((row, index) => {
     //   list1[index] = {make:row}
     // });
-    // console.log('sorting done');
-    // console.log('fixes', list1);
-    // console.log('#ofFixes', list1.length);
+    console.log('sorting done');
+    console.log('fixes', list1);
+    console.log('#ofFixes', list1.length);
     // console.log('badmakes', list2.length);
 
-    // const csv1 = papa.unparse(list1);
-    // fs.writeFile('dZoneMakes&modelsVer6.csv', csv1, (err) => {
-    //   if(err) console.log(err);
-    // })
+    const csv1 = papa.unparse(list1);
+    fs.writeFile('dZoneMakes&modelsVer6.csv', csv1, (err) => {
+      if(err) console.log(err);
+    })
     // const csv2 = papa.unparse(list2);
     // fs.writeFile('extraCommasVer4.csv', csv2, (err) => {
     //   if(err) console.log(err);
     // })
-    // console.log('writing done')
+    console.log('writing done')
 
     console.log('Success!')
   }
@@ -126,7 +95,7 @@ papa.parse(file, {
 //   'Moto-Guzzi': 'Moto Guzzi',
 //   'Moto-Ski': 'Moto-ski',
 //   'POLARIS': 'Polaris',
-//   'Piaggo': 'Piagigo',
+//   'Piaggo': 'Piaggio',
 //   'Recreatives': 'Recreatives Industries',
 //   'SKI-DOO': 'Ski-Doo',
 //   'SNO JET': 'Sno-Jet',
